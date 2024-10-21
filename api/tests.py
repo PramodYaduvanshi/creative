ab497f234c54982fa3fbb0406756ca6d77b2a1ed
Pramod K Yadav <mindvision1613@yahoo.com>
2024-10-21 21:57:31 +0530

    Test case done

--- api/tests.py
+++ api/tests.py
@@ -8,8 +8,8 @@ class APITestCase(TestCase):
         self.client = APIClient()
 
     def test_create_data(self):
-        response = self.client.post('/data/', {'DateTime': '2022-01-01 00:00:00'}, format='json')
+        response = self.client.post('/data/', {'10 Min Std dev': 10, 'Time': 1.76, 'DateTime': '5/01/2022 12:20:00', '10 Min Sampled Avg': 22.9}, format='json')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data['Date'], '01-January-2022')
+        self.assertEqual(response.data['Date'], '05-January-2022')
 
     # Add more tests for GET, PUT, DELETE
