04edebb1bdcab7a2390f89744df23a5cd94963de
Pramod K Yadav <mindvision1613@yahoo.com>
2024-10-22 13:05:24 +0530

    Test case done

--- api/tests.py
+++ api/tests.py
@@ -6,10 +6,27 @@ from rest_framework.test import APIClient
 class APITestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
+        self.data = {
+            '10 Min Std dev': 10,
+            'Time': 1.76,
+            'DateTime': '5/01/2022 12:20:00',
+            '10 Min Sampled Avg': 22.9
+            }
 
     def test_create_data(self):
-        response = self.client.post('/data/', {'10 Min Std dev': 10, 'Time': 1.76, 'DateTime': '5/01/2022 12:20:00', '10 Min Sampled Avg': 22.9}, format='json')
+        response = self.client.post('/data/', self.data, format='json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['Date'], '05-January-2022')
 
-    # Add more tests for GET, PUT, DELETE
+    def test_get_data(self):
+        response = self.client.get('/data/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_trigger_bog(self):
+        response = self.client.post('/trigger-job/')
+        print(response.data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_session_expiry(self):
+        response = self.client.get('/data-session-expiry/')
+        self.assertEqual(response.status_code, 200)
